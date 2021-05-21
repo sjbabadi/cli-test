2fd4cddf3b7618d95b5df5c2e49f6b288a87ce40
sjbabadi <sheila.babadi@yahoo.com>
2021-05-21 13:43:55 -0400

    alter styling

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const pool = new Pool({
   user: process.env.PG_USER,
   password: process.env.PG_PW,
-  host: "localhost",
+  host: process.env.PG_HOST || "localhost",
   port: 5432,
   database: "todos_db",
 });
